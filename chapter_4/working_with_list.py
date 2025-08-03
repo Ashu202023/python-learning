ac78e495bca98ce4d0352370b89477c5eea66981
Ashu202023 <ashutosh20kumar23@gmail.com>
2025-08-03 10:55:09 +0530

    learn more about list

--- chapter_4/working_with_list.py
+++ chapter_4/working_with_list.py
@@ -16,6 +16,35 @@ message="I love programming in python."
     #print(message) // this will raise an indentation error
 print(message)
 
-for car in cars 
-    print(car.title(), "is a great car.")  # this will raise a syntax error because of the missing colon at the end of the for loop.
-    
+# for car in cars
+    # print(car.title(), "is a great car.")  # this will raise a syntax error because of the missing colon at the end of the for loop.
+
+#making numerical lists 
+
+#using the range() function to generate a list of numbers
+for value in range(1,5): #// range(start,stop) generates numbers from start to stop-1
+    print(value)
+
+for x in  range(6): # // range(stop) generates numbers from 0 to stop-1
+    print(x)
+
+#using the range() function to create a list of numbers 
+
+numbers=list(range(1,6)) # creates a list of numbers from 1 to 5
+print(numbers)
+
+even_numbers=list(range(2,11,2))# creates a list of even numbers from 2 to 10
+print(even_numbers)
+
+squares=[]
+for value in range(1,11):
+    squares.append(value**2)
+print(squares)
+
+#simple statistics with a list of numbers 
+digits=[1,2,3,4,5,6,7,8,9,0]
+print(min(digits))  # minimum value in the list
+print(max(digits))  # maximum value in the list 
+print(sum(digits))  # sum of all values in the list
+
+#List comprehensions: a concise way to create lists 
